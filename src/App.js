07a95ebc4366e5a74e14961a648873c543fc4f04
kev5 <kevalk@bu.edu>
2018-03-08 12:59:11 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { List } from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Big Data Containers</h1>
+        </header>
+        <p className="App-intro" align="left">
+
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
